f5381b327ab0643c71ebf0f6fddbb5f7f9deae5f
Zach Stybel <zstybel@gmail.com>
2025-07-31 21:55:02 -0400

    updared vite congifuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  //base:"/Educa/",
   plugins: [react()],
+  base: './', // <-- Add this line
 })
