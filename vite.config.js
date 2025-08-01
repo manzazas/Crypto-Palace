b1f9218fe8ba702bb5573345a32e9be4f0d66dec
Zach Stybel <zstybel@gmail.com>
2025-07-31 21:52:55 -0400

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  //base:"/Educa/",
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Crypto-Palace"
-
 })
